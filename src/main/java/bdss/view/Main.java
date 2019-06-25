70f6fecfc20592c71ee1b80015934205365c113c
fluvios <noorfachrian@gmail.com>
2019-06-25 11:03:16 +0900

    add gui

---
+++ src/main/java/bdss/view/Main.java
@@ -0,0 +1,16 @@
+package bdss.view;
+
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+
+public class Main extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Main() {
+		setLayout(new GridLayout(1, 0, 0, 0));
+
+	}
+
+}
